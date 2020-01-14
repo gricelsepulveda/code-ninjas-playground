3bfa6bc407a93931bf1317295465d4fa298df4f5
Slowpokita <gricel@gocloud.ai>
2020-01-14 17:21:01 -0300

    ejemplo básico

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,9 +1,19 @@
 import React from "react"
 
 const Home = (props: any) => {
+
+  function greeter(person: string) {
+    return "Hello, " + person;
+  }
+
+  let user = "Gricel";
+
   return (
-    <div className="home">
-    </div>
+    <>
+      {
+        greeter(user)
+      }
+    </>
   )
 }
 
