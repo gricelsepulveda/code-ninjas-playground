b2c6a70dddadd414039f8756667c7380fcb691dd
Slowpokita <gricel@gocloud.ai>
2020-02-04 19:27:57 -0300

    Ejercicio update

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,21 +1,31 @@
 import React from "react"
 
-const Home = (props: any) => {
+const Home = () => {
 
-  function greeter(person: string) {
-    return "Hello, " + person;
+  const listaSupermercado = [
+    "2kg de naranja",
+    "1kg de limones",
+    "2 litros de leche",
+    "1 corrector"
+  ]
+
+
+  const imprimirLista = () => {
+    return listaSupermercado.map((hola) => 
+      (
+        <li>{hola}</li>
+      )
+    )
   }
-
-  let user = "Gricel";
 
   return (
     <>
-      {
-        greeter(user)
-      }
+      No te olvides de comprar:
+      <ul>
+      {imprimirLista()}
+      </ul>
     </>
   )
 }
-
 
 export default Home
